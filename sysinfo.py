d8b376cddba93978a07447424750e4df8726711a
Moritz Moeller <mm@mxs.de>
2019-11-07 09:53:59 +0100

    ..

--- sysinfo.py
+++ sysinfo.py
@@ -3,22 +3,49 @@ import psutil
 
 
 if __name__ == '__main__':
-    time.sleep(1)
+    processes = list(psutil.process_iter())
+    for process in processes:
+        try:
+            process.cpu_percent(None)
+        except psutil.AccessDenied:
+            pass
+
+    time.sleep(0.5)
+
+    print('psutil.cpu_freq', psutil.cpu_freq(True))
+
+    print('psutil.cpu_count', psutil.cpu_count(False), psutil.cpu_count(True))
+
+    print('psutil.cpu_times_percent', psutil.cpu_times_percent(0, True))
 
     cpu = psutil.cpu_percent(0, True)
+    print('psutil.cpu_percent', cpu)
     cpu_max = max(cpu)
     cpu_avg = sum(cpu) / len(cpu)
-    print(f"cpu max {cpu_max}% | avg {cpu_avg}%")
+    print(f"cpu max {cpu_max:.0f}% | avg {cpu_avg:.0f}%")
+    # which process?
 
+    # memory?
+    print('psutil.virtual_memory', psutil.virtual_memory())
 
-# get cpu usage
+    # check if high disk io?
+    # -> which process?
+    print('psutil.disk_io_counters', psutil.disk_io_counters(True))
 
-# get cpu usage per process
+    # check if high network io?
+    # -> which process?
+    print('psutil.net_io_counters', psutil.net_io_counters(True))
 
-# memory?
+    for process in processes:
+        try:
+            cpu_percent = process.cpu_percent(None)
+            if cpu_percent > 0:
+                print('process.cpu_percent', cpu_percent, process.pid, process.name())
+        except psutil.AccessDenied:
+            pass
 
-# check if high disk io?
-# -> which process?
-
-# check if high network io?
-# -> which process?
+    # for process in processes:
+    #     try:
+    #         print('process.memory_info', process.memory_info())
+    #     except psutil.AccessDenied:
+    #         pass
